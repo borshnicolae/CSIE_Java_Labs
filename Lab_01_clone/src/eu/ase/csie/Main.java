1a75d2882aac54de9bccc63d40002217073011c0
borshnicolae <nicolaebors@Nicolaes-MacBook-Pro.local>
2025-02-24 00:36:24 +0200

    lab_01 zip

--- Lab_01_clone/src/eu/ase/csie/Main.java
+++ Lab_01_clone/src/eu/ase/csie/Main.java
@@ -8,15 +8,15 @@ public class Main {
 		Client c1 = new Client(1, "Alice");
 		System.out.println("Client c1: id = " + c1.getId() + ", name = " + c1.getName());
 		//garbage collector
-		
+		System.out.println();
 		Client c2 = c1;
 		System.out.println("Client c1: id = " + c1.getId() + ", name = " + c1.getName());
 		System.out.println("Client c2: id = " + c2.getId() + ", name = " + c2.getName());
-		
+		System.out.println();
 		c2.setName("Bob");
 		System.out.println("Client c1: id = " + c1.getId() + ", name = " + c1.getName());
 		System.out.println("Client c2: id = " + c2.getId() + ", name = " + c2.getName());
-		
+		System.out.println();
 		c2 = c1.cloneUser();
 		c2.setId(2);
 		c2.setName("Alice");
@@ -26,8 +26,8 @@ public class Main {
 		
 		//String immutable //not necessary now
 		//java is call by value only
-		modUs(c2);
-		System.out.println("Client c2: id = " + c2.getId() + ", name = " + c2.getName());
+		//modUs(c2);
+		//System.out.println("Client c2: id = " + c2.getId() + ", name = " + c2.getName());
 	}
 
 	public static void modUs(Client u) {
